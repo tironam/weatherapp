46fd2b07db49e5e752e6811e42b60087fa5c29d0
Michael Tirona <tironamichael@gmail.com>
2020-05-04 19:04:11 -0700

    Added momentjs for date

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-fetch('https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={775ad81aec4e8e43e36233ea18090329}')
+fetch('https://api.openweathermap.org/data/2.5/weather?q=los angeles&appid=775ad81aec4e8e43e36233ea18090329')
     .then(r => r.json())
     .then(weather => {
         console.log(weather)
+        let currentWeather = document.getElementById('cityToday').innerHTML = `
+        <h2 class = "cityName">${weather.name} <span>${moment().format('MMMM Do, YYYY')}</span><h2>
+
+        `
     })

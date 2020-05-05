d82ad62fd436423a77b841b8ac3c75565da8fab3
Michael Tirona <tironamichael@gmail.com>
2020-05-04 18:22:47 -0700

    Trying to link api

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log('hey')
+fetch('https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={775ad81aec4e8e43e36233ea18090329}')
+    .then(r => r.json())
+    .then(weather => {
+        console.log(weather)
+    })
